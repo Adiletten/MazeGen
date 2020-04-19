ab834d7870dbad4948b2603af216e08fe7c3a5c3
Adiletten <luca@reschke.me>
2020-04-20 00:11:59 +0200

    Add: Basic System to Calc cell dimensions & drawing them. Very Basic.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+var cols, rows;
+var w = 40; // Cell Size
+var cells = [];
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  // For every row, and for each column in that row. Make a new cell
+  for (var x = 0; x < rows; x++) {
+    for (var y = 0; y < cols; y++) {
+      var cell = new Cell(y, x);
+      cells.push(cell);
+    }
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background(51);
+  // Draw each Cell
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].show(); // Show certain Cell
+  }
+}
+
+function Cell(y, x) {
+  this.x = x;
+  this.y = y;
+
+  // Show function for a specific cell
+  this.show = function() {
+    var x = this.x * w;
+    var y = this.y * w;
+    stroke(255);
+    noFill();
+    rect(x, y, w, w);
+  };
 }
