2026a4dd0ef311eb381a64d6ec74af4b005fce79
Muckie <luca@reschke.me>
2020-04-20 01:58:16 +0200

    Add: Tracking current cell Add: Cells have a visited property Add: Neighbours AND Edge Handling for neighbors :)

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var cols, rows;
 var w = 40; // Cell Size
 var cells = [];
+var current;
 
 function setup() {
   createCanvas(400, 400);
@@ -14,6 +15,9 @@ function setup() {
       cells.push(cell);
     }
   }
+
+  // Set current to the first cell
+  current = cells[0];
 }
 
 function draw() {
@@ -22,20 +26,55 @@ function draw() {
   for (var i = 0; i < cells.length; i++) {
     cells[i].show(); // Show certain Cell
   }
+
+  // Set current cell to visited
+  current.visited = true;
+  current.checkNeighbours();
+}
+
+function Index(y, x) {
+if (x < 0 || y < 0 || x > cols - 1 || y > rows - 1) {
+  return -1;
+}
+  return y + x * cols;
 }
 
 function Cell(y, x) {
   this.x = x;
   this.y = y;
   this.walls = [true, true, true, true]; // Top, Right, Bottom, Left
+  this.visited = false;
+
+  // Neighbour check for a specific cell
+  this.checkNeighbours = function() {
+    var neighbours = [];
+    var top = cells[Index(x, y - 1)];
+    var right = cells[Index(x + 1, y)];
+    var bottom = cells[Index(x, y + 1)];
+    var left = cells[Index(x - 1, y)];
+
+    if(top && !top.visited){
+      neighbours.push(top);
+    }
+    if(right && !right.visited){
+      neighbours.push(right);
+    }
+    if(bottom && !bottom.visited){
+      neighbours.push(bottom);
+    }
+    if(left && !left.visited){
+      neighbours.push(left);
+    }
+  };
 
   // Show function for a specific cell
   this.show = function() {
     var x = this.x * w;
     var y = this.y * w;
+
+    // Draw Walls
     stroke(255);
     if (this.walls[0]) {
-
       line(x, y, x + w, y); // Top line
     }
     if (this.walls[1]) {
@@ -48,7 +87,10 @@ function Cell(y, x) {
       line(x, y + w, x, y); // Left lines
     }
 
-    // noFill();
-    // rect(x, y, w, w);
+    // Colors cell if visited
+    if (this.visited) {
+      fill(255, 0, 200, 100);
+      rect(x, y, w, w);
+    }
   };
 }
